a6cdcaf8a8f7814cd34e5843d6f2a8e3e552f384
Christopher Youlou <youlou42@gmail.com>
2026-05-22 16:48:18 +0200

    Refactor deplacer function and add avancer and tourner

--- deplacer.cpp
+++ deplacer.cpp
@@ -1,34 +1,37 @@
 #include "Ktech_motor.h"
 
 
-void deplacer(float distance, float angle, float vitesse_roues){
-  float R = 0.025 ; // rayon des roues
-  float e = 20; // La moitié de la distance entre les roues                       i dont know????????????????????
-  float w_gauche = vitesse_roues; // vitesse de rotation du moteur gauche 
-  float w_droite = vitesse_roues;  // ~ droit
-  float t_debut, t_ecoule, t;
-  // tourner
-  t = angle / (-(R/e)*w_gauche + (R/e)*w_droite);
-  t_debut = micros();
-  t_ecoule = 0;
-  while (t_ecoule < t){
-    KTMOTORS[0].sendVelocityCommand((long int)(w_gauche), CAN);
-    KTMOTORS[1].sendVelocityCommand((long int)(w_droite), CAN);
-    t_ecoule = t_debut - micros();
-  }
-  KTMOTORS[0].sendVelocityCommand((long int)(0), CAN);
-  KTMOTORS[1].sendVelocityCommand((long int)(0), CAN);
+void avancer(float distance_m, int vitesse) {
+  unsigned long temps;
+  temps = (unsigned long)(fabs(distance_m) * MOVE_TIME_1_METER);
+  int signe = (distance_m > 0) ? 1 : -1;
+
+  KTMOTORS[0].sendVelocityCommand(signe * vitesse, CAN);
+  KTMOTORS[1].sendVelocityCommand(-signe * vitesse, CAN);
+
+  delay(temps);
+
+  KTMOTORS[0].sendVelocityCommand(0, CAN);
+  KTMOTORS[1].sendVelocityCommand(0, CAN);
+
+}
 
 
-  // avancer
-  t = distance / ((R/2)*w_gauche + (R/2)*w_droite);
-  t_debut = micros();
-  t_ecoule = 0;
-  while (t_ecoule < t){
-    KTMOTORS[0].sendVelocityCommand((long int)(-w_gauche), CAN);
-    KTMOTORS[1].sendVelocityCommand((long int)(w_droite), CAN);
-    t_ecoule = t_debut - micros();
-  }
-  KTMOTORS[0].sendVelocityCommand((long int)(0), CAN);
-  KTMOTORS[1].sendVelocityCommand((long int)(0), CAN);
+void tourner(float angle_deg, int vitesse) {
+  unsigned long temps;
+  temps = (unsigned long)((fabs(angle_deg) / 90.0) * TURN_TIME_90_DEG);
+  int signe = (angle_deg > 0) ? 1 : -1;
+
+  KTMOTORS[0].sendVelocityCommand(-signe * vitesse, CAN);
+  KTMOTORS[1].sendVelocityCommand(-signe * vitesse, CAN);
+
+  delay(temps);
+
+  KTMOTORS[0].sendVelocityCommand(0, CAN);
+  KTMOTORS[1].sendVelocityCommand(0, CAN);
+}
+
+void deplacer(float distance_m, float angle_deg, int vitesse){
+  tourner(angle_deg, vitesse);
+  avancer(distance_m, vitesse);
 }
