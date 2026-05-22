ea6be7ef77b1baa5e7cd536f1fb227215246a45e
Christopher Youlou <youlou42@gmail.com>
2026-05-22 17:10:08 +0200

    Add avancer and tourner functions to deplacer.h

--- deplacer.h
+++ deplacer.h
@@ -1,2 +1,3 @@
-
-void deplacer(float distance, float angle, float vitesse_roues);
+void avancer(float distance_m, int vitesse);
+void tourner(float angle_deg, int vitesse);
+void deplacer(float distance_m, float angle_deg, int vitesse);
